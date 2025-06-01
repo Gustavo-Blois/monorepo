cce8e01483ed733b6eb3ad0a9e2a65bec74b07e4
Gabriel Fontes <hi@m7.rs>
2025-06-01 13:19:51 +0200

    chore(flake): upgrade nixpkgs input to nixos-25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Infraestrutura principal para serviços hospedados pelo GELOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     hardware.url = "github:nixos/nixos-hardware";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
